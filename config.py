b3be33ad21553162a27159ac8e12d435eede0201
Yeyito <alinerob31@gmail.com>
2025-10-16 19:10:15 -0500

    Changed the name of catpuccin to be whale

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import catppuccin
+import whale
 
 # load your autoconfig, use this, if the rest of your config is empty!
 config.load_autoconfig()
@@ -6,4 +6,4 @@ config.load_autoconfig()
 # set the flavor you'd like to use
 # valid options are 'mocha', 'macchiato', 'frappe', and 'latte'
 # last argument (optional, default is False): enable the plain look for the menu rows
-catppuccin.setup(c, 'mocha', True)
+whale.setup(c)
