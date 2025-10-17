6eb7fcf1e7ecb1281b37310ba55d5906d285f08b
Yeyito <alinerob31@gmail.com>
2025-10-17 00:14:22 -0500

    Actually good whale theme

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 import whale
-
-# load your autoconfig, use this, if the rest of your config is empty!
 config.load_autoconfig()
-
-# set the flavor you'd like to use
-# valid options are 'mocha', 'macchiato', 'frappe', and 'latte'
-# last argument (optional, default is False): enable the plain look for the menu rows
 whale.setup(c)
+c.url.searchengines = {
+    'DEFAULT': 'https://www.google.com/search?hl=en&q={}',
+    'ddg': 'https://duckduckgo.com/?q={}',
+    'g': 'https://www.google.com/search?q={}',
+    'yt': 'https://www.youtube.com/results?search_query={}',
+    # … etc
+}
+
+config.bind('j', 'cmd-run-with-count 10 scroll down')
+config.bind('k', 'cmd-run-with-count 10 scroll up')
+c.content.javascript.clipboard = 'access'
+config.set("content.user_stylesheets", ["~/.config/qutebrowser/blue-theme.css"])
